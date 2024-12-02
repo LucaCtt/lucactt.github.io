301b85ceb9d2cd000cb07a505ac8b4644639a906
Luca Cotti <lucacotti@outlook.com>
2024-12-02 17:23:26 +0100

    feat: Add github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  site: "https://lucactt.github.io",
 });
